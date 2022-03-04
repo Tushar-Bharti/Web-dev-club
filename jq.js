e168df4d02d600e13563cc3d149e364eaa95f58b
Tushar Bharti <96988507+Tushar-Bharti@users.noreply.github.com>
2022-03-04 12:25:02 +0530

    Update jq.js

--- jq.js
+++ jq.js
@@ -7,11 +7,11 @@
         $(".introBlockHeader").hover(
       function() {
         $("#"+$(this).attr('id')).css({"box-shadow":"5px 5px 10px black"})
-        $("#"+$(this).attr('id')+"box").slideDown();
+        $("#"+$(this).attr('id')+"box").slideDown("slow");
       },
       function() {
                 $("#"+$(this).attr('id')).css({"box-shadow":"0px 0px 0px black"})
-        $("#"+$(this).attr('id')+"box").slideUp();
+        $("#"+$(this).attr('id')+"box").slideUp("slow");
       }
 )
 
@@ -19,11 +19,11 @@
        $(".introBlockHeaderOpposite").hover(
       function() {
                 $("#"+$(this).attr('id')).css({"box-shadow":"5px 5px 10px black"})
-        $("#"+$(this).attr('id')+"box").slideDown();
+        $("#"+$(this).attr('id')+"box").slideDown("slow");
       },
       function() {
                         $("#"+$(this).attr('id')).css({"box-shadow":"0px 0px 0px black"})
-        $("#"+$(this).attr('id')+"box").slideUp();
+        $("#"+$(this).attr('id')+"box").slideUp("slow");
       }
 )
 
